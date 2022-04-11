b73d43b52f095e22b36bd7413ce0638185d6b404
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-04-11 14:47:37 -0400

    finished

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    // this.length++
+    this.length = this.items.length;
 
-  get(pos) {}
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    }
+    // return Math.max(...this.items)
+    return this.items[this.length - 1];
+  }
 
-  sum() {}
+  min() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    }
+    // return Math.min(...this.items)
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce(function (total, nextVal) {
+      return total + nextVal;
+    }, 0);
+  }
+
+  avg() {
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
